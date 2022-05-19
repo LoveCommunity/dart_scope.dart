1d5c87a5702b200bf2b46044755b5f3dc93639d5
beeth0ven <beeth0vendev@gmail.com>
2022-05-19 10:24:05 +0800

    add `ObserverFunction`

---
+++ test/observables/observers/observer_test.dart
@@ -0,0 +1,29 @@
+
+import 'package:test/test.dart';
+
+import 'package:scopes/scopes.dart';
+
+
+void main() {
+
+  test('observer function invokes', () {
+    
+    final List<String> invokes = [];
+
+    final observer = Observer<String>((data) {
+      invokes.add(data);
+    });
+
+    expect(invokes, []);
+    observer.onData('a');
+    expect(invokes, [
+      'a',
+    ]);
+
+    observer.onData('b');
+    expect(invokes, [
+      'a',
+      'b',
+    ]);
+  });
+}
