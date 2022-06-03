9ee43b4113e7223d73396a784dba11e6aea996e7
beeth0ven <beeth0vendev@gmail.com>
2022-06-03 15:14:37 +0800

    add operator `observable.distinctMap`

--- test/observables/observables_test.dart
+++ test/observables/observables_test.dart
@@ -4,6 +4,7 @@ import 'observables/cast_observable_test.dart' as cast_observable_test;
 import 'observables/combine_observable_test.dart' as combine_observable_test;
 import 'observables/map_observable_test.dart' as map_observable_test;
 import 'observables/distinct_observable_test.dart' as distinct_observable_test;
+import 'observables/distinct_map_observable_test.dart' as distinct_map_observable_test;
 import 'observables/proxy_observable_test.dart' as proxy_observable_test;
 import 'observables/where_observable_test.dart' as where_observable_test;
 import 'observers/observer_test.dart' as observer_test;
@@ -14,6 +15,7 @@ void main() {
   cast_observable_test.main();
   combine_observable_test.main();
   distinct_observable_test.main();
+  distinct_map_observable_test.main();
   map_observable_test.main();
   proxy_observable_test.main();
   where_observable_test.main();
