8f6c8d651c671052914408c6432b38f934b09d62
beeth0ven <beeth0vendev@gmail.com>
2022-06-18 15:54:52 +0800

    rename `observable_combine_test.dart` to `combine_test.dart`

--- test/observables/observables/observable_combine_test.dart
+++ test/observables/observables/observable_combine_test.dart
@@ -1,7 +1,7 @@
 
 import 'package:test/test.dart';
 import 'package:scopes/scopes.dart';
-import '../../test_gen/combine_test/observable_combine_test.dart';
+import '../../test_gen/combine_test/combine_test.dart';
 import '../../toolbox/observable_tester.dart';
 
 part 'observable_combine_test.g.dart';
