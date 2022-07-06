46d177bdbe8f20410789c200539eb4dc837133f3
beeth0ven <beeth0vendev@gmail.com>
2022-07-06 15:14:13 +0800

    add `Configurable.combine`

--- lib/src/scopes/configurables/configurable.dart
+++ lib/src/scopes/configurables/configurable.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import '../scopes/configurable_scope.dart';
+import 'combine_configurable.dart';
 import 'configurable_function.dart';
 
 typedef ScopeConfigure = FutureOr<void> Function(ConfigurableScope scope);
@@ -9,5 +10,8 @@ abstract class Configurable {
   FutureOr<void> configure(ConfigurableScope scope);
 
   const factory Configurable(ScopeConfigure configure) = ConfigurableFunction;
+  const factory Configurable.combine({
+    required List<Configurable> children,
+  }) = CombineConfigurable;
 }
 
--- test/scopes/scopes_test.dart
+++ test/scopes/scopes_test.dart
@@ -1,4 +1,5 @@
 
+import 'configurables/configurable_combine_test.dart' as configurable_combine_test;
 import 'configurables/configurable_function_test.dart' as configurable_function_test;
 import 'scope_methods/disposable_test.dart' as disposable_test;
 import 'scope_methods/scope_get_test.dart' as scope_get_test;
@@ -6,6 +7,7 @@ import 'scope_methods/scope_push_test.dart' as scope_push_test;
 import 'scopes/scope_test.dart' as scope_test;
 
 void main() {
+  configurable_combine_test.main();
   configurable_function_test.main();
   disposable_test.main();
   scope_get_test.main();
