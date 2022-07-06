dd4c9517540ce8cbf21d30d558c66e3a26a03aa7
beeth0ven <beeth0vendev@gmail.com>
2022-07-06 22:03:39 +0800

    add `Final`

---
+++ lib/src/scopes/shared/typedefs.dart
@@ -0,0 +1,9 @@
+
+import 'package:typedef_foundation/typedef_foundation.dart';
+
+import '../scope_methods/scope_expose.dart';
+import '../scope_methods/scope_get.dart';
+
+typedef InstanceDispose<T> = void Function(T);
+typedef InstanceExpose<T> = void Function(ScopeExpose scope, Getter<T> getter);
+typedef Equal<T> = T Function(ScopeGet scope);
