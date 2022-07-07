e4691082c33e20c499d490d4af45ab60499732ff
beeth0ven <beeth0vendev@gmail.com>
2022-07-07 09:02:08 +0800

    fix typos

--- test/scopes_test.dart
+++ test/scopes_test.dart
@@ -1,10 +1,10 @@
 
 
-import 'observables/observables_test.dart' as observalbes_test;
+import 'observables/observables_test.dart' as observables_test;
 import 'scopes/scopes_test.dart' as scopes_test;
 
 void main() {
 
-  observalbes_test.main();
+  observables_test.main();
   scopes_test.main();
 }
