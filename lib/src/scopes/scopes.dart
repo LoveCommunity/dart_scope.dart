1d42f45a47c86153d1acc6d35246c3dfcbdb7534
beeth0ven <beeth0vendev@gmail.com>
2022-07-07 08:39:54 +0800

    add `scope.addDispose`

--- lib/src/scopes/scopes.dart
+++ lib/src/scopes/scopes.dart
@@ -2,6 +2,7 @@
 export 'configurables/configurable.dart';
 export 'configurables/final.dart';
 export 'errors/scope_value_not_exposed_error.dart';
+export 'scope_methods/disposable_sink.dart';
 export 'scope_methods/scope_get.dart';
 export 'scope_methods/scope_expose.dart';
 export 'scope_methods/scope_push.dart';
