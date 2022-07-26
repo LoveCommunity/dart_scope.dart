cf147a9d49ac0f1e4f7699a7db10b1a33e3896f4
beeth0ven <beeth0vendev@gmail.com>
2022-07-26 10:02:05 +0800

    add configurable `Computed2`

--- lib/src/scopes/configurables/computed.dart
+++ lib/src/scopes/configurables/computed.dart
@@ -11,13 +11,34 @@ class Computed<T, R> extends FinalStatesBase<R> {
   Computed({
     Object? name,
     Object? statesName,
-    required R Function(ScopeGet scope, T) compute,
+    required R Function(ScopeGet scope, T it) compute,
     Equals<R>? equals,
     bool late = true,
   }): super(
     name: name,
     equal: _superEqual<T, R>(
       statesName,
+      compute,
+      equals,
+    ),
+    late: late,
+  );
+}
+
+class Computed2<T1, T2, R> extends FinalStatesBase<R> {
+
+  Computed2({
+    Object? name,
+    Object? statesName1,
+    Object? statesName2,
+    required R Function(ScopeGet scope, T1 it1, T2 it2) compute,
+    Equals<R>? equals,
+    bool late = true,
+  }): super(
+    name: name,
+    equal: _superEqual2<T1, T2, R>(
+      statesName1,
+      statesName2,
       compute,
       equals,
     ),
@@ -37,3 +58,19 @@ Equal<States<R>> _superEqual<T, R>(
       equals: equals,
     );
 }
+
+Equal<States<R>> _superEqual2<T1, T2, R>(
+  Object? statesName1,
+  Object? statesName2,
+  R Function(ScopeGet scope, T1 it1, T2 it2) compute,
+  Equals<R>? equals,
+) {
+  return (scope) => States
+    .combine2<T1, T2, R>(
+      states1: scope.get<States<T1>>(name: statesName1),
+      states2: scope.get<States<T2>>(name: statesName2),
+      combiner: (it1, it2) => compute(scope, it1, it2),
+    )
+    .distinct(equals);
+}
+
