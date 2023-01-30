70e32c68c32399d65160b6465eacdea979a51817
beeth0ven <beeth0vendev@gmail.com>
2023-01-30 10:03:32 +0800

    reimplements `ObservableMulticast` using `Observation`

--- lib/src/dart_observable/observables/observable_multicast.dart
+++ lib/src/dart_observable/observables/observable_multicast.dart
@@ -3,6 +3,7 @@ import 'package:meta/meta.dart';
 import 'package:disposal/disposal.dart';
 
 import 'observable.dart';
+import 'observation.dart';
 import '../observers/observer.dart';
 import '../subjects/subject.dart';
 import '../subjects/publisher.dart';
@@ -14,41 +15,81 @@ class ObservableMulticast<T> implements Observable<T> {
     Subject<T> Function()? createSubject,
     required Observable<T> observable,
   }): _createSubject = createSubject ?? _defaultCreateSubject,
-    _observable = observable;
+    _observable = observable,
+    _shared = _SharedBetweenObservations<T>();
 
   final Subject<T> Function() _createSubject;
   final Observable<T> _observable;
 
-  int _observationsLength = 0;
-  Subject<T>? _subject;
-  Disposable? _connection;
+  final _SharedBetweenObservations<T> _shared;
 
   @override
   Disposable observe(OnData<T> onData) {
-    bool disposed = false;
-    final subject = _subject ??= _createSubject();
-    final observation = subject.observe(onData);
-    _observationsLength += 1;
-    if (_observationsLength == 1) {
-      _connection = _observable.observe(subject.onData);
-    }
-    return Disposable(() {
-      if (disposed) {
-        return;
-      }
-      disposed = true;
-      observation.dispose();
-      _observationsLength -= 1;
-      if (_observationsLength == 0) {
-        _connection?.dispose();
-        _connection = null;
-        _subject?.dispose();
-        _subject = null;
-      }
-    });
+    return _Observation<T>(
+      createSubject: _createSubject,
+      observable: _observable,
+      shared: _shared,
+      emit: onData,
+    );
   }
 }
 
 Subject<T> _defaultCreateSubject<T>() {
   return Publisher<T>();
+}
+
+class _SharedBetweenObservations<T> {
+  int observationsLength = 0;
+  Subject<T>? subject;
+  Disposable? observation;
+}
+
+class _Observation<T> extends Observation<T> {
+
+  _Observation({
+    required Subject<T> Function() createSubject,
+    required Observable<T> observable,
+    required _SharedBetweenObservations<T> shared,
+    required OnData<T> emit,
+  }): _createSubject = createSubject,
+    _observable = observable,
+    _shared = shared,
+    super(emit: emit);
+
+  final Subject<T> Function() _createSubject;
+  final Observable<T> _observable;
+
+  final _SharedBetweenObservations<T> _shared;
+
+  bool _disposed = false;
+  late final Disposable _subjectObservation;
+
+  @override
+  void init() {
+    final subject = _shared.subject ??= _createSubject();
+    _subjectObservation = subject.observe(emit);
+    _shared.observationsLength += 1;
+    if (_shared.observationsLength == 1) {
+      _shared.observation = _observable.observe(subject.onData);
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_disposed) {
+      return;
+    }
+    _disposed = true;
+    final shouldDisposeSharedObservation = _shared.observationsLength == 1;
+    if (shouldDisposeSharedObservation) {
+      _shared.observation?.dispose();
+      _shared.observation = null;
+    }
+    _subjectObservation.dispose();
+    _shared.observationsLength -= 1;
+    if (shouldDisposeSharedObservation) {
+      _shared.subject?.dispose();
+      _shared.subject = null;
+    }
+  }
 }
