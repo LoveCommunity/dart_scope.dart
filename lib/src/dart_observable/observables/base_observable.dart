6162fd3725a7e7e3a759f7eaeb08ec33f8d6be1d
beeth0ven <beeth0vendev@gmail.com>
2023-03-06 09:08:09 +0800

    add `MultiSourcePipeObservable` api documentation

--- lib/src/dart_observable/observables/base_observable.dart
+++ lib/src/dart_observable/observables/base_observable.dart
@@ -49,11 +49,52 @@ abstract class PipeObservable<T, R> implements Observable<R> {
   final Observable<T> source;
 }
 
+/// `MultiSourcePipeObservable` is a pipeline that transform multiple input
+/// observables to an output observable.
+/// 
+/// The multiple input observables is a list of source,
+/// each source implements `Observable<T>`,
+/// and the output observable is itself that implements `Observable<R>`.
+/// 
+/// ```dart
+/// abstract class MultiSourcePipeObservable<T, R> implements Observable<R> {
+///
+///   const MultiSourcePipeObservable({
+///     required this.sources,
+///   });
+/// 
+///   final List<Observable<T>> sources; 
+/// }
+/// ```
+/// 
+/// Example:
+/// 
+/// ```dart
+/// class ObservableCombine<T, R> extends MultiSourcePipeObservable<T, R> {
+/// 
+///   const ObservableCombine({
+///     required super.sources,
+///     required this.combiner,
+///   });
+/// 
+///   final R Function(List<T> items) combiner;
+/// 
+///   @override
+///   Disposable observe(OnData<R> onData) {  ... }
+/// }
+/// ```
+/// 
+/// `ObservableCombine<T, R>` is a `MultiSourcePipeObservable`, 
+/// since it transform multiple input `Observable<T>` 
+/// to an output `Observable<R>`.
+/// 
 abstract class MultiSourcePipeObservable<T, R> implements Observable<R> {
 
+  /// Create a `MultiSourcePipeObservable` with sources.
   const MultiSourcePipeObservable({
     required this.sources,
   });
-  
+
+  /// The input observables
   final List<Observable<T>> sources; 
 }
