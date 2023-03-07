eca121334f09dde9f00e75a9485284a7abcef96b
beeth0ven <beeth0vendev@gmail.com>
2023-03-07 10:08:31 +0800

    add test for `InstanceAsObservable`

---
+++ test/dart_observable/observables/instance_as_observable_test.dart
@@ -0,0 +1,26 @@
+
+import 'package:test/test.dart';
+import 'package:dart_scope/dart_scope.dart';
+
+void main() {
+
+  test('`InstanceAsObservable` common', () {
+
+    final stream = Stream.fromIterable([0, 1, 2]);
+    final observable = _StreamAsObservable(stream);
+
+    final isIdentical = identical(stream, observable.instance);
+
+    expect(isIdentical, true);
+  });
+}
+
+class _StreamAsObservable<T> extends InstanceAsObservable<Stream<T>, T> {
+
+  _StreamAsObservable(super.instance);
+
+  @override
+  Disposable observe(OnData<T> onData) {
+    throw UnimplementedError();
+  }
+}
